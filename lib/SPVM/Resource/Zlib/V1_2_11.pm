e50dcee1c0c2cfe26de2a7d0056bc0fcd3a828dd
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-15 16:37:48 +0900

    First commit

---
+++ lib/SPVM/Resource/Zlib/V1_2_11.pm
@@ -0,0 +1,49 @@
+package SPVM::Resource::Zlib::V1_2_11;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+SPVM::Resource::Zlib::V1_2_11 - Resource::Zlib::V1_2_11 is a SPVM module
+
+=head1 SYNOPSYS
+
+  use Resource::Zlib::V1_2_11;
+  
+=head1 DESCRIPTION
+
+Resource::Zlib::V1_2_11 is a SPVM module.
+
+=head1 STATIC METHODS
+
+
+
+=head1 INSTANCE METHODS
+
+
+
+=head1 REPOSITORY
+
+
+
+=head1 BUG REPORT
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright YYYY-YYYY AUTHOR_NAME, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
